bd141995be24525f2498786af93726149bc38c06
fzkfzkqq <31615678+fzkfzkqq@users.noreply.github.com>
2019-08-27 14:54:54 +1000

    historic bushfire in heat map

--- app/src/main/java/com/example/mapbox/Historic.java
+++ app/src/main/java/com/example/mapbox/Historic.java
@@ -20,6 +20,8 @@ import com.mapbox.android.core.location.LocationEngineResult;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.modes.CameraMode;
@@ -28,12 +30,37 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.CircleLayer;
+import com.mapbox.mapboxsdk.style.layers.HeatmapLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.heatmapDensity;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.interpolate;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.linear;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.rgb;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.rgba;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.stop;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.zoom;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleOpacity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleRadius;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleStrokeColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.circleStrokeWidth;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.heatmapColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.heatmapIntensity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.heatmapOpacity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.heatmapRadius;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.heatmapWeight;
 
 public class Historic extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
     private PermissionsManager permissionsManager;
@@ -41,13 +68,16 @@ public class Historic extends AppCompatActivity implements OnMapReadyCallback, P
     private MapboxMap map;
     private String postCode;
     private LocationEngine locationEngine;
-    private TextView risk;
     private LocationChangeListeningActivityLocationCallback callback =
             new LocationChangeListeningActivityLocationCallback(this);
     private static final long DEFAULT_INTERVAL_IN_MILLISECONDS = 3000L;
     private static final long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 500;
     private JSONObject j = new JSONObject();
-
+    private static final String EARTHQUAKE_SOURCE_URL = "https://disastermateapi.azurewebsites.net/historicalbushfires.json";
+    private static final String EARTHQUAKE_SOURCE_ID = "earthquakes";
+    private static final String HEATMAP_LAYER_ID = "earthquakes-heat";
+    private static final String HEATMAP_LAYER_SOURCE = "earthquakes";
+    private static final String CIRCLE_LAYER_ID = "earthquakes-circle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +86,6 @@ public class Historic extends AppCompatActivity implements OnMapReadyCallback, P
         setContentView(R.layout.activity_historic);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        risk = (TextView) findViewById(R.id.text_riskrate);
-
 //        getDetailAsyncTask getDetailAsyncTask =new getDetailAsyncTask();
 //        getDetailAsyncTask.execute("3125");
 
@@ -65,10 +93,13 @@ public class Historic extends AppCompatActivity implements OnMapReadyCallback, P
             @Override
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
                 map = mapboxMap;
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                mapboxMap.setStyle(Style.DARK, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-                        enableLocationComponent(style);
+                        addEarthquakeSource(style);
+                        addHeatmapLayer(style);
+                        addCircleLayer(style);
+                        //enableLocationComponent(style);
 // Map is set up and the style has loaded. Now you can add data or make other map adjustments
                     }
                 });
@@ -87,7 +118,125 @@ public class Historic extends AppCompatActivity implements OnMapReadyCallback, P
 
     }
 
+    private void addEarthquakeSource(@NonNull Style loadedMapStyle) {
+        try {
+            loadedMapStyle.addSource(new GeoJsonSource(EARTHQUAKE_SOURCE_ID, new URI(EARTHQUAKE_SOURCE_URL)));
+            Log.i("geojson",loadedMapStyle.getSource(EARTHQUAKE_SOURCE_ID).toString());
+        } catch (URISyntaxException uriSyntaxException) {
+            Log.e("error",uriSyntaxException.getMessage());
+        }
+    }
 
+    private void addHeatmapLayer(@NonNull Style loadedMapStyle) {
+        HeatmapLayer layer = new HeatmapLayer(HEATMAP_LAYER_ID, EARTHQUAKE_SOURCE_ID);
+        layer.setMaxZoom(9);
+        layer.setSourceLayer(HEATMAP_LAYER_SOURCE);
+        layer.setProperties(
+
+// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+// Begin color ramp at 0-stop with a 0-transparency color
+// to create a blur-like effect.
+                heatmapColor(
+                        interpolate(
+                                linear(), heatmapDensity(),
+                                literal(0), rgba(33, 102, 172, 0),
+                                literal(0.2), rgb(103, 169, 207),
+                                literal(0.4), rgb(209, 229, 240),
+                                literal(0.6), rgb(253, 219, 199),
+                                literal(0.8), rgb(239, 138, 98),
+                                literal(1), rgb(178, 24, 43)
+                        )
+                ),
+
+// Increase the heatmap weight based on frequency and property magnitude
+                heatmapWeight(
+                        interpolate(
+                                linear(), get("power"),
+                                stop(0, 0),
+                                stop(6, 1)
+                        )
+                ),
+
+// Increase the heatmap color weight weight by zoom level
+// heatmap-intensity is a multiplier on top of heatmap-weight
+                heatmapIntensity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(0, 1),
+                                stop(9, 3)
+                        )
+                ),
+
+// Adjust the heatmap radius by zoom level
+                heatmapRadius(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(0, 2),
+                                stop(9, 20)
+                        )
+                ),
+
+// Transition from heatmap to circle layer by zoom level
+                heatmapOpacity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(7, 1),
+                                stop(9, 0)
+                        )
+                )
+        );
+
+        loadedMapStyle.addLayerAbove(layer, "waterway-label");
+    }
+
+    private void addCircleLayer(@NonNull Style loadedMapStyle) {
+        CircleLayer circleLayer = new CircleLayer(CIRCLE_LAYER_ID, EARTHQUAKE_SOURCE_ID);
+        circleLayer.setProperties(
+
+// Size circle radius by earthquake magnitude and zoom level
+                circleRadius(
+                        interpolate(
+                                linear(), zoom(),
+                                literal(7), interpolate(
+                                        linear(), get("power"),
+                                        stop(1, 1),
+                                        stop(6, 4)
+                                ),
+                                literal(16), interpolate(
+                                        linear(), get("power"),
+                                        stop(1, 5),
+                                        stop(6, 50)
+                                )
+                        )
+                ),
+
+// Color circle by earthquake magnitude
+                circleColor(
+                        interpolate(
+                                linear(), get("power"),
+                                literal(1), rgba(33, 102, 172, 0),
+                                literal(2), rgb(103, 169, 207),
+                                literal(3), rgb(209, 229, 240),
+                                literal(4), rgb(253, 219, 199),
+                                literal(5), rgb(239, 138, 98),
+                                literal(6), rgb(178, 24, 43)
+                        )
+                ),
+
+// Transition from heatmap to circle layer by zoom level
+                circleOpacity(
+                        interpolate(
+                                linear(), zoom(),
+                                stop(7, 0),
+                                stop(8, 1)
+                        )
+                ),
+                circleStrokeColor("white"),
+                circleStrokeWidth(1.0f)
+        );
+
+        loadedMapStyle.addLayerBelow(circleLayer, HEATMAP_LAYER_ID);
+    }
     @Override
     public void onStart() {
         super.onStart();
