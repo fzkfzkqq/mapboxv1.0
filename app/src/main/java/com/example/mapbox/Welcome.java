be0db4fbf8fae4568442b27880d11dc90c1bb2c2
Nikhil Paliath <npal0004@student.monash.edu>
2019-08-28 12:52:46 +1000

    Disclaimer Logic fixed

--- app/src/main/java/com/example/mapbox/Welcome.java
+++ app/src/main/java/com/example/mapbox/Welcome.java
@@ -1,16 +1,21 @@
 package com.example.mapbox;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+
 
 
 /**
@@ -87,24 +92,39 @@ public class Welcome extends AppCompatActivity {
         }
     };
 
+    private static String PREFS_NAME = "Prefs";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         setContentView(R.layout.activity_welcome);
 
         mVisible = true;
         mContentView = findViewById(R.id.fullscreen_content);
 
-        //TODO: need to fix the size of the fullscreen image
+        SharedPreferences sharedpreferences;
+        sharedpreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(Welcome.this, MainActivity.class);
-                startActivity(i);
-            }
-        }, TIME_OUT);
+        Boolean isAgree = sharedpreferences.getBoolean("accepted",false);
+
+        if(!isAgree) {
+            //TODO: need to fix the size of the fullscreen image
+            showDialog(0);
+
+        }
+        else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(Welcome.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }, TIME_OUT);
+        }
     }
 
 
@@ -122,6 +142,63 @@ public class Welcome extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        // show disclaimer....
+        // for example, you can show a dialog box...
+
+        SharedPreferences sharedpreferences;
+        sharedpreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        Boolean isAgree = sharedpreferences.getBoolean("accepted",false);
+
+        if(!isAgree){
+
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(" LEGAL DISCLAIMER \n\n While we make every effort to ensure that material on this application is accurate and up to date (unless denoted as archived material), such material does in no way constitute the provision of professional advice or a prediction that should be relied upon.\n" +
+                    "\n" +
+                    "DisasterMate does not guarantee, and accepts no legal liability whatsoever arising from or connected to, the accuracy, reliability, currency or completeness of any material contained on this application or any linked site. Users should seek appropriate independent professional advice prior to relying on or entering into any commitment based on material published here, which material is purely published for reference purposes alone.\n" +
+                    "\n" +
+                    "By using the application, you will be deemed to have released and discharged DisasterMate from all liability (including negligence) for all claims, losses expenses, damages, and costs the user may incur as a result of relying on the information on this application, including liability in respect of any defamatory material on any database or in respect of any dealings with any work (including software) in which you hold any copyright or other intellectual property right. By using the application, you will be assuming all risks associated with the use of this application, including risks to your phone, software or data by any virus which might be transmitted, downloaded or activated via this or an external website and/or its contents.\n" +
+                    "\n" +
+                    "Material, summarised views, standards or recommendations of third parties or recommendations made by this application do not necessarily reflect the views of DisasterMate or indicate a commitment to a course of action.\n")
+                    .setCancelable(false)
+                    .setPositiveButton("Agree", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // and, if the user accept, you can execute something like this:
+                            // We need an Editor object to make preference changes.
+                            // All objects are from android.context.Context
+                            SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.putBoolean("accepted", true);
+                            // Commit the edits!
+                            editor.commit();
+                            Intent i = new Intent(Welcome.this, MainActivity.class);
+                            startActivity(i);
+
+                        }
+                    })
+                .setNegativeButton("Disagree", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //nm.cancel(R.notification.running);
+                        // cancel the NotificationManager (icon)
+                        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean("accepted", false);
+                        // Commit the edits!
+                        finish();
+
+                    }
+                });
+            AlertDialog alert = builder.create();
+            return alert;
+        }
+        else
+            return null;
+    }
 
 
     @Override
